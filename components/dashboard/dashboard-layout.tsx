4e64fdf209e5e8fd783553360bbafce46a937e3b
Ariful Islam <hellomrariful@GMAIL.COM>
2025-06-30 22:17:41 +0600

    Updated dashboard-layout.tsx

--- components/dashboard/dashboard-layout.tsx
+++ components/dashboard/dashboard-layout.tsx
@@ -20,14 +20,6 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { 
-  DropdownMenu, 
-  DropdownMenuContent, 
-  DropdownMenuItem, 
-  DropdownMenuLabel, 
-  DropdownMenuSeparator, 
-  DropdownMenuTrigger 
-} from '@/components/ui/dropdown-menu';
 import { cn } from '@/lib/utils';
 
 const navigation = [
